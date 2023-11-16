c2ca87997f367d9ab1e9f504c5ed11efa2847f4f
SOFYAPALMA <116221329+SOFYAPALMA@users.noreply.github.com>
2023-11-16 14:46:01 -0500

    Update Formulario424_Encabezado.cs
    encabezado

--- CapaModelo/Formulario424_Encabezado.cs
+++ CapaModelo/Formulario424_Encabezado.cs
@@ -8,11 +8,24 @@ namespace CapaModelo
 {
    public class Formulario424_Encabezado
     {
-        public int Entidad { get; set; }
         public int Tipo { get; set; }
         public int Codigo { get; set; }
-        public int Nombre { get; set; }
-        public int FechaHora { get; set; }
-        public int tipo { get; set; }
+        public String Nombre { get; set; }
+        public DateTime FechaHora { get; set; }
+        public String NombreComercial { get; set; }
+        public int TipodeProductoDeposito { get; set; }
+        public int AperturaDigital { get; set; }
+        public int NumerodeClientesUnicos { get; set; }
+        public int CuotadeManejo { get; set; }
+        public int ObservacionesCuotadeManejo { get; set; }
+        public int GrupoPoblacional { get; set; }
+        public int Ingresos { get; set; }
+        public int ServicioGratuitoCuentadeAhorros1 { get; set; }
+        public int ServicioGratuitoCuentadeAhorros2 { get; set; }
+        public int ServicioGratuitoCuentadeAhorros3 { get; set; }
+        public int ServicioGratuitoTarjetaDebito1 { get; set; }
+        public int ServicioGratuitoTarjetaDebito2 { get; set; }
+        public int ServicioGratuitoTarjetaDebito3 { get; set; }
+
     }
 }
