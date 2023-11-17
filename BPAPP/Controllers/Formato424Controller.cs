718af6d26488cc6a43f04eeef69d35a19c271182
Robert Pineda <admin@robertpineda.com>
2023-11-17 11:22:05 -0500

    controller
    cambios view

--- BPAPP/Controllers/Formato424Controller.cs
+++ BPAPP/Controllers/Formato424Controller.cs
@@ -43,13 +43,85 @@ namespace ProyectoWeb.Controllers
         /// </summary>
         private void LlenadoListas()
         {
-            List<Dominio> dominios = CD_Dominios.Obtener(1);
+            List<Dominio> tipodeProductoDeposito = CD_Dominios.Obtener(1);
+            List<Dominio> aperturaDeposito = CD_Dominios.Obtener(2);
+            List<Dominio> grupoPoblacional = CD_Dominios.Obtener(3);
+            List<Dominio> ingresos = CD_Dominios.Obtener(4);
+            List<Dominio> observacionesCuotadeManejo = CD_Dominios.Obtener(5);
+            List<Dominio> servicioGratuitoCuentadeAhorros1 = CD_Dominios.Obtener(6);
+            List<Dominio> servicioGratuitoCuentadeAhorros2 = CD_Dominios.Obtener(6);
+            List<Dominio> servicioGratuitoCuentadeAhorros3 = CD_Dominios.Obtener(6);
+            List<Dominio> servicioGratuitoTarjetaDebito1 = CD_Dominios.Obtener(9);
+            List<Dominio> servicioGratuitoTarjetaDebito2 = CD_Dominios.Obtener(9);
+            List<Dominio> servicioGratuitoTarjetaDebito3 = CD_Dominios.Obtener(9);
 
-            if (dominios.Count() == 0)
+
+            if (tipodeProductoDeposito.Count() == 0)
             {
-                ModelState.AddModelError("Tipo", "No se encuentra valores para la lista de tipo de entidades");
+                ModelState.AddModelError("TipodeProductoDeposito", "No se encuentra valores para la lista de tipo de producto deposito");
             }
-            ViewBag.TipoEntidad = new SelectList(dominios, "IdDominio", "Nombre");
+            ViewBag.TipodeProductoDeposito = new SelectList(tipodeProductoDeposito, "IdDominio", "Nombre");
+
+            if (aperturaDeposito.Count() == 0)
+            {
+                ModelState.AddModelError("AperturaDeposito", "No se encuentra valores para la lista de Apertura Deposito");
+            }
+            ViewBag.AperturaDeposito = new SelectList(aperturaDeposito, "IdDominio", "Nombre");
+
+            if (grupoPoblacional.Count() == 0)
+            {
+                ModelState.AddModelError("GrupoPoblacional", "No se encuentra valores para la lista de Grupo Poblacional");
+            }
+            ViewBag.GrupoPoblacional = new SelectList(grupoPoblacional, "IdDominio", "Nombre");
+
+            if (ingresos.Count() == 0)
+            {
+                ModelState.AddModelError("Ingresos", "No se encuentra valores para la lista de Ingresos");
+            }
+            ViewBag.Ingresos = new SelectList(ingresos, "IdDominio", "Nombre");
+
+            if (observacionesCuotadeManejo.Count() == 0)
+            {
+                ModelState.AddModelError("ObservacionesCuotadeManejo", "No se encuentra valores para la lista de Observaciones Cuota de Manejo");
+            }
+            ViewBag.ObservacionesCuotadeManejo = new SelectList(observacionesCuotadeManejo, "IdDominio", "Nombre");
+
+            if (servicioGratuitoCuentadeAhorros1.Count() == 0)
+            {
+                ModelState.AddModelError("ServicioGratuitoCuentadeAhorros1", "No se encuentra valores para la lista de Servicio Gratuito Cuenta de Ahorros1");
+            }
+            ViewBag.ServicioGratuitoCuentadeAhorros1 = new SelectList(servicioGratuitoCuentadeAhorros1, "IdDominio", "Nombre");
+
+            if (servicioGratuitoCuentadeAhorros2.Count() == 0)
+            {
+                ModelState.AddModelError("ServicioGratuitoCuentadeAhorros2", "No se encuentra valores para la lista de Servicio Gratuito Cuenta de Ahorros2");
+            }
+            ViewBag.ServicioGratuitoCuentadeAhorros2 = new SelectList(servicioGratuitoCuentadeAhorros2, "IdDominio", "Nombre");
+
+            if (servicioGratuitoCuentadeAhorros3.Count() == 0)
+            {
+                ModelState.AddModelError("ServicioGratuitoCuentadeAhorros3", "No se encuentra valores para la lista de Servicio Gratuito Cuenta de Ahorros1");
+            }
+            ViewBag.ServicioGratuitoCuentadeAhorros3 = new SelectList(servicioGratuitoCuentadeAhorros3, "IdDominio", "Nombre");
+
+            if (servicioGratuitoTarjetaDebito1.Count() == 0)
+            {
+                ModelState.AddModelError("ServicioGratuitoTarjetaDebito1", "No se encuentra valores para la lista de Servicio Gratuito Tarjeta Debito1");
+            }
+            ViewBag.ServicioGratuitoTarjetaDebito1 = new SelectList(servicioGratuitoTarjetaDebito1, "IdDominio", "Nombre");
+
+            if (servicioGratuitoTarjetaDebito2.Count() == 0)
+            {
+                ModelState.AddModelError("ServicioGratuitoTarjetaDebito2", "No se encuentra valores para la lista de Servicio Gratuito Tarjeta Debito2");
+            }
+            ViewBag.ServicioGratuitoTarjetaDebito2 = new SelectList(servicioGratuitoTarjetaDebito2, "IdDominio", "Nombre");
+
+            if (servicioGratuitoTarjetaDebito3.Count() == 0)
+            {
+                ModelState.AddModelError("ServicioGratuitoTarjetaDebito3", "No se encuentra valores para la lista de Servicio Gratuito Tarjeta Debito3");
+            }
+            ViewBag.ServicioGratuitoTarjetaDebito3 = new SelectList(servicioGratuitoTarjetaDebito3, "IdDominio", "Nombre");
+
         }
 
         [HttpPost]
