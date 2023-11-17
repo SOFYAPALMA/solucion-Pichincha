e403591cde9960a9d23593a67f06c81b63dbd653
Robert Pineda <admin@robertpineda.com>
2023-11-16 22:26:08 -0500

    cambios varios

--- CapaModelo/Formulario424_Encabezado.cs
+++ CapaModelo/Formulario424_Encabezado.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CapaModelo
 {
-   public class Formulario424_Encabezado
+    public class Formulario424_Encabezado
     {
         public int Tipo { get; set; }
         public int Codigo { get; set; }
