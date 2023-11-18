b018a7ac00fb9c9fa6702b3c9d7c146703d2c1ca
SOFYAPALMA <116221329+SOFYAPALMA@users.noreply.github.com>
2023-11-18 15:00:44 -0500

    consulta

--- BPAPP/Models/Form424ConsultaEncabezado.cs
+++ BPAPP/Models/Form424ConsultaEncabezado.cs
@@ -5,24 +5,29 @@ namespace ProyectoWeb.Models
 {
     public class Form424ConsultaEncabezado
     {
+        [Display(Name = "Fecha Hora")]
         public DateTime FechaHora { get; set; }
+
+        [Display(Name = "Apertura Digital")]
         public string AperturaDigital { get; set; }
+
+        [Display(Name = "Codigo")]
         public string Codigo { get; set; }
         public string CodigoRegistro { get; set; }
+
+        [Display(Name = "Cuota Manejo")]
         public int CuotaManejo { get; set; }
         public int Estado { get; set; }
-
-        [Display(Name = "Fecha Actualizacion")]
         public string Fecha_horaActualizacion { get; set; }
         public string Fechacorte { get; set; }
         public string FechaEstado { get; set; }
+
+        [Display(Name = "Grupo Poblacional")]
         public string GrupoPoblacional { get; set; }
         public int idAperturaDigital { get; set; }
         public int idGrupoPoblacional { get; set; }
         public int idIngresos { get; set; }
         public int idObservacionesCuota { get; set; }
-
-        [Display(Name = "ID")]
         public int idPropiedadesFormato { get; set; }
         public int? idPropiedadesFormatoAnterior { get; set; }
         public int idSerGratuito_CtaAHO { get; set; }
@@ -32,23 +37,46 @@ namespace ProyectoWeb.Models
         public int idSerGratuito_TCRDebito2 { get; set; }
         public int idSerGratuito_TCRDebito3 { get; set; }
         public int idTipoProductoDeposito { get; set; }
+
+        [Display(Name = "Ingresos")]
         public string Ingresos { get; set; }
+
+        [Display(Name = "Nombre Entidad")]
         public string Nombre { get; set; }
 
         [Display(Name = "Nombre Comercial")]
         public string NombreComercial { get; set; }
+
+        [Display(Name = "Numero clientes")]
         public int NumeroClientes { get; set; }
+
+        [Display(Name = "Observaciones Cuota")]
         public string ObservacionesCuota { get; set; }
 
         [Display(Name = "Servicio Gratuito Cuenta de Ahorros 1")]
         public string SerGratuito_CtaAH1 { get; set; }
+
+        [Display(Name = "Servicio Gratuito Cuenta de Ahorros 2")]
         public string SerGratuito_CtaAH2 { get; set; }
+
+        [Display(Name = "Servicio Gratuito Cuenta de Ahorros 3")]
         public string SerGratuito_CtaAH3 { get; set; }
+
+        [Display(Name = "Servicio Tarjeta Debito 1")]
         public string SerGratuito_TCRDebito1 { get; set; }
+
+        [Display(Name = "Servicio Tarjeta Debito 2")]
         public string SerGratuito_TCRDebito2 { get; set; }
+
+        [Display(Name = "Servicio Tarjeta Debito 3")]
         public string SerGratuito_TCRDebito3 { get; set; }
+
+        [Display(Name = "Tipo Entidad")]
         public string Tipo { get; set; }
+
+        [Display(Name = "Tipo de Producto Deposito")]
         public string TipoProductoDeposito { get; set; }
         public string Usuario { get; set; }
+
     }
 }
