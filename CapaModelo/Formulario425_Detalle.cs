3afefe4d637959893b9853b566b3042ce3b97e94
SOFYAPALMA <116221329+SOFYAPALMA@users.noreply.github.com>
2023-11-19 20:30:54 -0500

    425

--- CapaModelo/Formulario425_Detalle.cs
+++ CapaModelo/Formulario425_Detalle.cs
@@ -8,17 +8,29 @@ namespace CapaModelo
 {
     public class Formulario425_Detalle
     {
-        public int Subcuentas { get; set; }
-        public int OperacionoServicio { get; set; }
+        public int idDetalle { get; set; }
+        public string Detalle { get; set; }
+        public int idPropiedadesFormato { get; set; }
+        public string PropiedadesFormato { get; set; }
+        public int Subcuenta { get; set; }
+        public int idOperacionoServicio { get; set; }
+        public string OperacionoServicio { get; set; }
         public int Canal { get; set; }
         public int CostoFijo { get; set; }
         public int CostoFijoMaximo { get; set; }
-        public int CostoProporcionalalaOperacionoServicio { get; set; }
-        public int CostoProporcionalMaximoalaOperacionoServicio { get; set; }
-        public char Tasa { get; set; }
-        public char TasaMaxima { get; set; }
-        public int TipoAseguradora { get; set; }
-        public int CodigoAseguradora { get; set; }
-        public int Observaciones { get; set; }
+        public int CostoProporcionOperacionServicio { get; set; }
+        public int CostoProporcionMaxOperacionServicio { get; set; }
+        public int Tasa { get; set; }
+        public int TasaMaxima { get; set; }
+        public int idTipoAseguradora { get; set; }
+        public string TipoAseguradora { get; set; }
+        public int idCodigoAseguradora { get; set; }
+        public string CodigoAseguradora { get; set; }
+        public int idObservaciones { get; set; }
+        public string Observaciones { get; set; }
+        public int UnidadCaptura { get; set; }
+        public int Estado { get; set; }
+        public int FechaProceso { get; set; }
+        public int FechaEstado { get; set; }
     }
 }
