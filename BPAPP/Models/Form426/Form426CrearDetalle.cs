7811dc9a8110262b4995a373569be9443baa7ebc
SOFYAPALMA <116221329+SOFYAPALMA@users.noreply.github.com>
2023-11-20 23:00:09 -0500

    426

--- BPAPP/Models/Form426/Form426CrearDetalle.cs
+++ BPAPP/Models/Form426/Form426CrearDetalle.cs
@@ -1,20 +1,62 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProyectoWeb.Models
 {
     public class Form426CrearDetalle
     {
         public string Detalle { get; set; }
+
+        [Required(ErrorMessage = "El campo  ID Detalle es obligatorio.")]
+        [Display(Name = "ID Detalle")]
         public int idDetalle { get; set; }
+
+        [Required(ErrorMessage = "El campo  Propiedades Formato es obligatorio.")]
+        [Display(Name = "Propiedades Formato")]
+        [StringLength(50)]
+        [DataType(DataType.Text)]
         public string PropiedadesFormato { get; set; }
+
+        [Required(ErrorMessage = "El campo  Propiedades Formato es obligatorio.")]
+        [Display(Name = "Propiedades Formato")]
         public int idPropiedadesFormato { get; set; }
+
+        [Required(ErrorMessage = "El campo Subcuenta es obligatorio.")]
+        [Display(Name = "Subcuenta")]
         public int Subcuenta { get; set; }
+
+        [Required(ErrorMessage = "El campo Caracteristica Credito es obligatorio.")]
+        [Display(Name = "Caracteristica Credito")]
+        [StringLength(50)]
+        [DataType(DataType.Text)]
         public string CaracteristicaCredito { get; set; }
+
+        [Required(ErrorMessage = "El campo Caracteristica Credito es obligatorio.")]
+        [Display(Name = "Caracteristica Credito")]
         public int idCaracteristicaCredito { get; set; }
+
+        [Required(ErrorMessage = "El campo Costo es obligatorio.")]
+        [Display(Name = "Costo")]
         public int Costo { get; set; }
+
+        [Required(ErrorMessage = "El campo Tasa es obligatorio.")]
+        [Display(Name = "Tasa")]
         public int Tasa { get; set; }
+
+        [Required(ErrorMessage = "El campo Tipo Aseguradora es obligatorio.")]
+        [Display(Name = "Tipo Aseguradora")]
+        [StringLength(50)]
+        [DataType(DataType.Text)]
         public string TipoAseguradora { get; set; }
+
+        [Required(ErrorMessage = "El campo Tipo Aseguradora es obligatorio.")]
+        [Display(Name = "Tipo Aseguradora")]
         public int idTipoAseguradora { get; set; }
+
+        [Required(ErrorMessage = "El campo Codigo Aseguradora es obligatorio.")]
+        [Display(Name = "Codigo Aseguradora")]
+        [StringLength(50)]
+        [DataType(DataType.Text)]
         public string CodigoAseguradora { get; set; }
         public int idCodigoAseguradora { get; set; }
         public string Observaciones { get; set; }
