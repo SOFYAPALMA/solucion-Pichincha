5ea9a0fe0f92ae420e048567fc88797b2c96e356
SOFYAPALMA <116221329+SOFYAPALMA@users.noreply.github.com>
2023-12-03 19:54:17 -0500

    dominio

---
+++ BPAPP/Controllers/DominioController.cs
@@ -0,0 +1,27 @@
+﻿using ProyectoWeb.Models.Dominio;
+using System;
+using System.Web.Mvc;
+
+namespace ProyectoWeb.Controllers
+{
+    [Authorize]
+    public class DominioController : Controller
+    {
+        public ActionResult Crear()
+        {
+            DominioCrearEncabezado dominio = new DominioCrearEncabezado();
+            LlenadoListasDominio();
+
+            return View(dominio);
+        }
+
+        public ActionResult CrearDetalle(int id)
+        {
+            DominioCrearEncabezado dominio = new DominioCrearEncabezado();
+            dominio.idDominio = id;
+            LlenadoListasDetalle();
+
+            return View(dominio);
+        }
+    }
+}
---
+++ BPAPP/Models/Dominio/DominioCrearEncabezado.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoWeb.Models.Dominio
+{
+    public class DominioCrearEncabezado
+    {
+        /// <summary>
+        /// Referencia a al tipo entidad
+        /// </summary>
+        /// 
+        [Display(Name = "Dominio")]
+        public int Dominio { get; set; }
+        public int idDominioGen { get; set; }
+        public int idDominio { get; set; }
+
+        [Display(Name = "Descripcion")]
+        public string Descripcion { get; set; }
+        public int idCodigo { get; set; }
+
+        [Display(Name = "Fecha")]
+        public string Fecha { get; set; }
+
+        [Display(Name = "Estado")]
+        public string Estado { get; set; }
+
+    }
+}
